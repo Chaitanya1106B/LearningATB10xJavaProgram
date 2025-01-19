48f032c0733df49e768dae7db504e659247fd0e1
Chaitanya <b.chaitanya1106@gmail.com>
2025-01-19 19:21:15 +0530

    19th-Jan

---
+++ src/ex_01_Java_Basic/Lab_001_HelloWorld.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab_001_HelloWorld {
+    public static void main (String[] args) {    //shortcut - main
+        System.out.println("HelloWorld!");       //sout
+    }
+}
